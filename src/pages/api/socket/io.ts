0c31f5ce8569949c6fdd40ab9d0b0dce9cefcbcc
Tania <pashkevichtania2004@gmail.com>
2023-11-12 16:52:10 +0300

    add socket to pages api

---
+++ src/pages/api/socket/io.ts
@@ -0,0 +1,66 @@
+import { Server } from "socket.io"
+import type { Server as HTTPServer } from "http"
+import type { NextApiRequest, NextApiResponse } from "next"
+import type { Socket as NetSocket } from "net"
+import type { Server as IOServer } from "socket.io"
+import { SocketEvents } from "@/types/enums"
+
+interface SocketServer extends HTTPServer {
+  io?: IOServer | undefined
+}
+
+interface SocketWithIO extends NetSocket {
+  server: SocketServer
+}
+
+interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+function ioHandler(req: NextApiRequest, res: NextApiResponseWithSocket) {
+  if (!res.socket.server.io) {
+    const path = "/api/socket/io"
+    const httpServer = res.socket.server
+    const io = new Server(httpServer, {
+      path,
+      addTrailingSlash: false,
+      allowEIO3: true,
+      transports: ["websocket", "polling"],
+    })
+    res.socket.server.io = io
+
+    // Event handler for client connections
+    io.on("connection", (socket) => {
+      const clientId = socket.id
+
+      console.log("A user connected:", clientId)
+
+      socket.on(SocketEvents.JoinRoom, (roomId) => {
+        socket.join(roomId)
+        console.log(`User with id-${clientId} joined room - ${roomId}`)
+      })
+
+      socket.on(SocketEvents.SendMessage, (data) => {
+        console.log("Message send, data: ", data)
+        // This will send a message to a specific room ID
+        // to sends to all in room except sender
+        // in sends to all in room including sender
+        socket.to(data.roomId).emit(SocketEvents.ReceiveMessage, data)
+      })
+
+      socket.on(SocketEvents.Disconnect, () => {
+        console.log("A user disconnected:", clientId)
+      })
+    })
+  }
+
+  res.end()
+}
+
+export default ioHandler
--- src/webSocket/socketClientClass.ts
+++ src/webSocket/socketClientClass.ts
@@ -5,17 +5,22 @@ import type { DefaultEventsMap } from "@socket.io/component-emitter"
 export default class SocketSingleton {
   private static instance: Socket<DefaultEventsMap, DefaultEventsMap>
 
-  private static ENDPOINT = process.env.NEXT_PUBLIC_SERVER_URL as string
-
   static getInstance() {
+    console.log("Get instance", this.instance)
     if (!this.instance) {
       console.log("Connect to websocket server")
-      this.instance = io(
-          this.ENDPOINT,
-          {
-            // autoConnect: true,
-            transports: ["websocket", "polling"]
-          })
+      // To create socket server
+      fetch("/api/socket/io")
+      this.instance = io(process.env.NEXT_PUBLIC_BASE_URL!, {
+        path: "/api/socket/io",
+        addTrailingSlash: false,
+        // autoConnect: true,
+        transports: ["websocket", "polling"],
+      })
+
+      this.instance.on("connect_error", (e) => {
+        console.log(e)
+      })
     }
 
     return this.instance
